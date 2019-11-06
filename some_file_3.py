20b9aa869e3540b4fc74554386706c5f59599a6d
Pedro Costa <pedro.costa@sage.com>
2019-11-06 11:45:36 +0000

    test 2

--- some_file_3.py
+++ some_file_3.py
@@ -1,2 +1,3 @@
 'd1bc8d3ba4afc7e109612cb73acbdddac052c93025aa1f82942edabb7deb82a1'
 'd1bc8d3ba4afc7e109612cb73acbdddac052c93025aa1f82942edabb7deb82a1'
+'d1bc8d3ba4afc7e109612cb73acbdddac052c93025aa1f82942edabb7deb82a1'
